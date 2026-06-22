f3a857a0948c9d74a89090b7aa45f5fdfcc034ca
etcenterprise <etcenterprise24@gmail.com>
2026-06-22 12:17:16 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,60 @@
-module.exports = function(eleventyConfig) {
+const fs = require("fs");
+const yaml = require("js-yaml");
+const matter = require("gray-matter");
+
+module.exports = function (eleventyConfig) {
 
   // Copy static files
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("logo.png");
-  eleventyConfig.addPassthroughCopy("_content");
 
-  // Projects collection
-  eleventyConfig.addCollection("projects", function(collectionApi) {
+  // Collections
+  eleventyConfig.addCollection("projects", function (collectionApi) {
     return collectionApi.getFilteredByGlob("_projects/*.md");
   });
 
-  // Services collection
-  eleventyConfig.addCollection("services", function(collectionApi) {
+  eleventyConfig.addCollection("services", function (collectionApi) {
     return collectionApi.getFilteredByGlob("_services/*.md");
   });
 
-  // Audit collection
-  eleventyConfig.addCollection("audit", function(collectionApi) {
+  eleventyConfig.addCollection("audit", function (collectionApi) {
     return collectionApi.getFilteredByGlob("_audit/*.md");
   });
+
+  // Global CMS data
+  function loadMarkdownData(file) {
+    const fileContents = fs.readFileSync(file, "utf8");
+    return matter(fileContents).data;
+  }
+
+  eleventyConfig.addGlobalData(
+    "home",
+    loadMarkdownData("./_content/home.md")
+  );
+
+  eleventyConfig.addGlobalData(
+    "about",
+    loadMarkdownData("./_content/about.md")
+  );
+
+  eleventyConfig.addGlobalData(
+    "contact",
+    loadMarkdownData("./_content/contact.md")
+  );
+
+  eleventyConfig.addGlobalData(
+    "gallery",
+    loadMarkdownData("./_content/gallery.md")
+  );
+
+  eleventyConfig.addGlobalData(
+    "navigation",
+    yaml.load(
+      fs.readFileSync("./_content/navigation.yml", "utf8")
+    )
+  );
 
   return {
     dir: {
