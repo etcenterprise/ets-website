88361fb972c51264a2043aa18021bf8b69da9130
etcenterprise <etcenterprise24@gmail.com>
2026-06-22 11:05:44 +0300

    Fix homepage and keep navigation CMS

--- .eleventy.js
+++ .eleventy.js
@@ -1,45 +1,30 @@
 module.exports = function(eleventyConfig) {
 
+  // Copy static files
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("logo.png");
 
+  // Projects collection
   eleventyConfig.addCollection("projects", function(collectionApi) {
     return collectionApi.getFilteredByGlob("_projects/*.md");
   });
 
+  // Services collection
   eleventyConfig.addCollection("services", function(collectionApi) {
     return collectionApi.getFilteredByGlob("_services/*.md");
   });
 
+  // Audit collection
   eleventyConfig.addCollection("audit", function(collectionApi) {
     return collectionApi.getFilteredByGlob("_audit/*.md");
   });
 
+  // Navigation data from CMS
   eleventyConfig.addGlobalData(
     "navigation",
     require("./_content/navigation.yml")
-  );
-
-  eleventyConfig.addGlobalData(
-    "home",
-    require("./_content/home.md")
-  );
-
-  eleventyConfig.addGlobalData(
-    "about",
-    require("./_content/about.md")
-  );
-
-  eleventyConfig.addGlobalData(
-    "contact",
-    require("./_content/contact.md")
-  );
-
-  eleventyConfig.addGlobalData(
-    "gallery",
-    require("./_content/gallery.md")
   );
 
   return {
