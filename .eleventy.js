31bf2acfa89081026906c7ecdb1ec0205cc81fc8
etcenterprise <etcenterprise24@gmail.com>
2026-06-23 11:34:02 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 module.exports = function(eleventyConfig) {
 
+  // Static files
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("logo.png");
 
+  // Collections
   eleventyConfig.addCollection("projects", function(collectionApi) {
     return collectionApi.getFilteredByGlob("_projects/*.md");
   });
@@ -20,7 +22,8 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: ".",
-      includes: "_includes"
+      includes: "_includes",
+      data: "_content"
     }
   };
 };
