b9ee5495ee768e68b004ea049fd6489476d83a66
etcenterprise <etcenterprise24@gmail.com>
2026-06-23 09:43:17 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,60 +1,26 @@
-const fs = require("fs");
-const yaml = require("js-yaml");
-const matter = require("gray-matter");
+module.exports = function(eleventyConfig) {
 
-module.exports = function (eleventyConfig) {
-
-  // Copy static files
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("logo.png");
 
-  // Collections
-  eleventyConfig.addCollection("projects", function (collectionApi) {
+  eleventyConfig.addGlobalData("home", require("./_content/home.md"));
+  eleventyConfig.addGlobalData("about", require("./_content/about.md"));
+  eleventyConfig.addGlobalData("contact", require("./_content/contact.md"));
+  eleventyConfig.addGlobalData("gallery", require("./_content/gallery.md"));
+
+  eleventyConfig.addCollection("projects", function(collectionApi) {
     return collectionApi.getFilteredByGlob("_projects/*.md");
   });
 
-  eleventyConfig.addCollection("services", function (collectionApi) {
+  eleventyConfig.addCollection("services", function(collectionApi) {
     return collectionApi.getFilteredByGlob("_services/*.md");
   });
 
-  eleventyConfig.addCollection("audit", function (collectionApi) {
+  eleventyConfig.addCollection("audit", function(collectionApi) {
     return collectionApi.getFilteredByGlob("_audit/*.md");
   });
-
-  // Global CMS data
-  function loadMarkdownData(file) {
-    const fileContents = fs.readFileSync(file, "utf8");
-    return matter(fileContents).data;
-  }
-
-  eleventyConfig.addGlobalData(
-    "home",
-    loadMarkdownData("./_content/home.md")
-  );
-
-  eleventyConfig.addGlobalData(
-    "about",
-    loadMarkdownData("./_content/about.md")
-  );
-
-  eleventyConfig.addGlobalData(
-    "contact",
-    loadMarkdownData("./_content/contact.md")
-  );
-
-  eleventyConfig.addGlobalData(
-    "gallery",
-    loadMarkdownData("./_content/gallery.md")
-  );
-
-  eleventyConfig.addGlobalData(
-    "navigation",
-    yaml.load(
-      fs.readFileSync("./_content/navigation.yml", "utf8")
-    )
-  );
 
   return {
     dir: {
