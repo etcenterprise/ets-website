be3f1961e4912dd708cfc130f6467139657267a3
etcenterprise <etcenterprise24@gmail.com>
2026-06-26 16:22:03 +0300

    Update home.js

--- _data/home.js
+++ _data/home.js
@@ -1,1 +1,7 @@
+const fs = require("fs");
+const matter = require("gray-matter");
 
+module.exports = () => {
+  const file = fs.readFileSync("./_content/home.md", "utf8");
+  return matter(file).data;
+};
