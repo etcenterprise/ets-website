9310939c364ca70ef828d637d8043826c23c7c21
etcenterprise <etcenterprise24@gmail.com>
2026-08-02 13:09:54 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,54 @@
 module.exports = function(eleventyConfig) {
 
-  // Static files
+  // =========================
+  // Static Files
+  // =========================
+
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("logo.png");
 
-  // Collections
+
+  // =========================
+  // Engineering Projects
+  // =========================
+
   eleventyConfig.addCollection("projects", function(collectionApi) {
     return collectionApi.getFilteredByGlob("_projects/*.md");
   });
+
+
+  // =========================
+  // Engineering Services
+  // =========================
 
   eleventyConfig.addCollection("services", function(collectionApi) {
     return collectionApi.getFilteredByGlob("_services/*.md");
   });
 
+
+  // =========================
+  // Audit Projects / Services
+  // =========================
+
   eleventyConfig.addCollection("audit", function(collectionApi) {
     return collectionApi.getFilteredByGlob("_audit/*.md");
   });
+
+
+  // =========================
+  // Audit Team
+  // =========================
+
+  eleventyConfig.addCollection("audit_team", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("_audit_team/*.md");
+  });
+
+
+  // =========================
+  // Eleventy Settings
+  // =========================
 
   return {
     dir: {
@@ -26,4 +57,5 @@ module.exports = function(eleventyConfig) {
       data: "_data"
     }
   };
+
 };
